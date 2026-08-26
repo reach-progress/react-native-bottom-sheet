e4044a2d77d715a51a3d6ad761c762f3c146784a
Justin Parker <justin@jpark.tech>
2026-08-26 13:06:13 -0600

    feat: restore bottom sheet text input integration

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-export { default } from './components/bottomSheet';
-export { BottomSheetScrollView } from './components/bottomSheetScrollable';
-export { useBottomSheet } from './hooks/useBottomSheet';
-
 export type { BottomSheetProps } from './components/bottomSheet';
+export { default } from './components/bottomSheet';
 export type {
   BottomSheetScrollViewMethods,
   BottomSheetScrollViewProps,
 } from './components/bottomSheetScrollable';
+export { BottomSheetScrollView } from './components/bottomSheetScrollable';
+export type { BottomSheetTextInputProps } from './components/bottomSheetTextInput';
+export { default as BottomSheetTextInput } from './components/bottomSheetTextInput';
+export { useBottomSheet } from './hooks/useBottomSheet';
